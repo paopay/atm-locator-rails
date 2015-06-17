3ed4f4c4bbb3f52f125f291ed54dd489ed4ba211
Paolo Payumo <paolo.payumo@yahoo.com>
2015-06-17 10:53:28 -0700

    - fix some css - clean up

--- app/assets/javascripts/components/map.js.jsx
+++ app/assets/javascripts/components/map.js.jsx
@@ -189,21 +189,7 @@ var GoogleMap = React.createClass({
     },
     render: function () {
         return (
-                <div className='googleMap'></div>
+            <div className='googleMap'></div>
         );
     }
 });
-
-
-
-// <div>
-
-//   <!-- Nav tabs -->
-//   <ul className="nav nav-tabs" role="tablist">
-//     <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Home</a></li>
-//     <li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Profile</a></li>
-//     <li role="presentation"><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab">Messages</a></li>
-//     <li role="presentation"><a href="#settings" aria-controls="settings" role="tab" data-toggle="tab">Settings</a></li>
-//   </ul>
-
-// </div>
